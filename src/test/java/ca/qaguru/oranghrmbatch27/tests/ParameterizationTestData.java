fbb5e6e31fb483849319fae6266254314fbc6eaa
than <thanexplore@gmail.com>
2023-09-16 20:13:42 +0530

    Sample Initial POM project with Login and Education Tests

---
+++ src/test/java/ca/qaguru/oranghrmbatch27/tests/ParameterizationTestData.java
@@ -0,0 +1,13 @@
+package ca.qaguru.oranghrmbatch27.tests;
+
+import org.testng.annotations.DataProvider;
+
+public class ParameterizationTestData {
+    @DataProvider(name = "LoginInfo")
+    public Object[][] getUserInfo(){
+        return new Object[][]{
+                {"joe", "joe123"},
+                {"Hary", "Password"}
+        };
+    }
+}
